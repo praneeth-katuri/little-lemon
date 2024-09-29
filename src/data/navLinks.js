4ef22ff720b21f9c3ac84ba12323a73de0a391cc
praneeth-katuri <praneethkaturi@outlook.com>
2024-09-29 18:33:11 +0530

    chore: Create external data array to pass to Navbar

---
+++ src/data/navLinks.js
@@ -0,0 +1,10 @@
+const navLinks = [
+  { name: "Home", to: "/" },
+  { name: "About", to: "/about" },
+  { name: "Menu", to: "/menu" },
+  { name: "Reservations", to: "/reservation" },
+  { name: "Order", to: "/order" },
+  { name: "Login", to: "/login" },
+];
+
+export default navLinks;
